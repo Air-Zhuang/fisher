d61397a592d3ff1b2c42bfa08f6998e299618a13
Air-Zhuang <737248514@qq.com>
2018-09-08 15:14:39 +0800

    1.0

--- fisher.py
+++ fisher.py
@@ -1,5 +1,8 @@
-from flask import Flask,make_response
+import json
+from flask import Flask
+
 from helper import is_isbn_or_key
+from yushu_book import YuShuBook
 
 app=Flask(__name__)
 app.config.from_object('config')
@@ -7,7 +10,11 @@ app.config.from_object('config')
 @app.route('/book/search/<q>/<page>')   #这样会识别成请求参数
 def hello(q,page):
     '''
-        api:http://t.yushu.im/v2/book/search?q={}&start={}&count={}
+        doubanapi:
+            http://t.yushu.im/v2/book/search?q={}&count={}&start={}
+            http://api.douban.com/v2/book/isbn/{isbn}
+        api:
+            http://t.yushu.im/v2/book/search?q={}&count={}&start={}
             http://t.yushu.im/v2/book/isbn/{isbn}
         q:普通关键字/isbn
         page
@@ -15,6 +22,12 @@ def hello(q,page):
              isbn10 10个0到9数字组成，含有一些'-'  (旧)
     '''
     isbn_or_key=is_isbn_or_key(q)
+    if isbn_or_key=='isbn':
+        result=YuShuBook.search_by_isbn(q)
+    else:
+        result=YuShuBook.search_by_keyword(q)
+    return json.dumps(result),200,{'content-type':'application/json'}
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',debug=app.config['DEBUG'])
