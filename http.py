ceb0d63435635a9db328c1206e3b525d9b646725
Air-Zhuang <737248514@qq.com>
2018-09-08 13:17:30 +0800

    1.0

--- http.py
+++ http.py
@@ -1,5 +1,11 @@
 import requests
 
+
 class HTTP:
-    def get(self,url,return_json=True):
-        pass
+    '''通过requests库发送HTTP请求'''
+
+    def get(self, url, return_json=True):
+        r = requests.get(url)
+        if r.status_code != 200:                            # 兼容200的格式
+            return {} if return_json else ''
+        return r.json() if return_json else r.text()        # 将结果转换成json格式
