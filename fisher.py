444e6c8639240dbe12870d2d6bd2d4915c0133a6
Air-Zhuang <737248514@qq.com>
2018-09-12 09:31:18 +0800

    1.0

--- fisher.py
+++ fisher.py
@@ -5,7 +5,7 @@ from app import create_app
 app=create_app()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',debug=app.config['DEBUG'])
+    app.run(host='0.0.0.0',debug=app.config['DEBUG'],threaded=True)
     '''
     http://127.0.0.1:5000/book/search/郭敬明/1
     http://t.yushu.im/v2/book/isbn/9787501524044
