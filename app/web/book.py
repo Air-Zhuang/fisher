d7c8f33324efdaf25ec707836fe8aaebac95fae5
Air-Zhuang <737248514@qq.com>
2018-09-12 21:04:10 +0800

    1.0

--- app/web/book.py
+++ app/web/book.py
@@ -3,6 +3,7 @@ from flask import jsonify,request
 from app.forms.book import SearchForm
 from app.libs.helper import is_isbn_or_key
 from app.spider.yushu_book import YuShuBook
+from app.view_models.book import BookViewModel,BookCollection
 from . import web
 
 
@@ -21,14 +22,17 @@ def hello():
              isbn10 10个0到9数字组成，含有一些'-'  (旧)
     '''
     form=SearchForm(request.args)
+    books=BookCollection()
     if form.validate():                 #SearchForm验证通过会返回True,否则返回False
         q=form.q.data.strip()
         page=form.page.data
         isbn_or_key=is_isbn_or_key(q)
+        yushu_book = YuShuBook()
         if isbn_or_key=='isbn':
-            result=YuShuBook.search_by_isbn(q)
+            yushu_book.search_by_isbn(q)
         else:
-            result=YuShuBook.search_by_keyword(q,page)
-        return jsonify(result)
+            yushu_book.search_by_keyword(q,page)
+        books.fill(yushu_book,q)
+        return jsonify(books)
     else:
         return jsonify(form.errors)     #WTForms验证不通过会将错误信息放在errors属性中
