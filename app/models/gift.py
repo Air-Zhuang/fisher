c35cbac613d87eb2f1fc0838b48733c48088e586
Air-Zhuang <737248514@qq.com>
2018-09-14 10:10:41 +0800

    1.0

--- app/models/gift.py
+++ app/models/gift.py
@@ -1,7 +1,10 @@
 from sqlalchemy import Column
 from sqlalchemy import Integer,String,Float,Boolean
+from sqlalchemy.orm import relationship
 from app.models.base import db
 
 class Gift(db.Model):
     id=Column(Integer,primary_key=True)
+    user=relationship('User')
+    uid=Column(Integer)
     launched=Column(Boolean,default=False)
